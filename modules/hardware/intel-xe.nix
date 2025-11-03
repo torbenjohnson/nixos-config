41a6fcc568ba917ea53598cec9b1e3e4829edb15
Torben Johnson <tjohnson@dimonoff.com>
2025-11-03 09:11:34 -0500

    remove unavailable cli tools

--- modules/hardware/intel-xe.nix
+++ modules/hardware/intel-xe.nix
@@ -10,8 +10,5 @@
   hardware.graphics.extraPackages = with pkgs; [
     intel-media-driver
     vpl-gpu-rt
-    intel-gpu-tools # Intel GPU monitoring
-    nvtopPackages.intel # GPU process viewer
-    libva-utils # VA-API utilities
   ];
 }
