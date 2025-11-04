f46033b09a19b8406a39eecc9b27d4cdda11c274
Torben Johnson <tjohnson@dimonoff.com>
2025-11-04 11:05:59 -0500

    add sound service microphones to hosts

--- hosts/lp-0098/configuration.nix
+++ hosts/lp-0098/configuration.nix
@@ -27,6 +27,13 @@
 
   networking.hostName = "lp-0098";
 
+  networking.hosts = {
+    "192.168.103.189" = [ "micro-1" ];
+    "192.168.103.188" = [ "micro-2" ];
+    "192.168.103.187" = [ "micro-3" ];
+    "192.168.103.186" = [ "micro-4" ];
+  };
+
   services.hardware.bolt.enable = true;
 
   system.stateVersion = "25.05";
