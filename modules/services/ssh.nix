9978b28e31a329c56b0ef2f6af98bbd28a168bd0
Torben Johnson <tjohnson@dimonoff.com>
2025-11-29 14:42:14 -0500

    improve sshd settings

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -4,5 +4,13 @@
   services.openssh = {
     enable = true;
     startWhenNeeded = true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+      MaxAuthTries = 10;
+      X11Forwarding = false;
+      StrictModes = true;
+    };
   };
 }
