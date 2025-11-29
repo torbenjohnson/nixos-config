9a03fef09c85cf3dfe0ff76b55bed08c7fdb103e
Torben Johnson <tjohnson@dimonoff.com>
2025-11-29 15:14:24 -0500

    add lp-0098 public ssh key

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -20,6 +20,7 @@
       shell = pkgs.fish;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG+ZBqp/rH4MlosNVibyeDlifZ7XfTXbnGTBz8XebA24 tor@psi"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG+ZBqp/rH4MlosNVibyeDlifZ7XfTXbnGTBz8XebA24 tor@lp-0098"
       ];
     };
   };
