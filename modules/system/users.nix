9c1a97678ed6ab54553409fca0da5e0ef22f1fbb
Torben Johnson <tjohnson@dimonoff.com>
2025-11-29 15:17:48 -0500

    add eta public ssh key

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -20,6 +20,7 @@
       shell = pkgs.fish;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8q/ckMwsbktbOk4rQnme8OPlqPVnILSCqSO/nc+FfO tor@psi"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFW6A4ymM5pZqTUCiYlstpj3hfna9EsvclnDPpMKpJOi tor@eta"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG+ZBqp/rH4MlosNVibyeDlifZ7XfTXbnGTBz8XebA24 tor@lp-0098"
       ];
     };
