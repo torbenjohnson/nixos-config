ae18f044943961f65dcb4b38510615fd8d8a013c
Torben Johnson <tjohnson@dimonoff.com>
2025-12-02 15:50:17 -0500

    add rust and go bin to path

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -10,3 +10,6 @@ set -x ELECTRON_OZONE_PLATFORM_HINT wayland
 set -Ux EDITOR hx
 set -Ux VISUAL hx
 set -Ux SUDO_EDITOR
+
+set -gx PATH /home/tor/.cargo/bin $PATH
+set -gx PATH /home/tor/go/bin $PATH
