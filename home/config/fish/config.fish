9832ebe0d06c08ec735daefa12d59cbde8693f40
Torben Johnson <tjohnson@dimonoff.com>
2025-12-02 12:52:22 -0500

    move theme and direnv to is-interactive for fish

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -1,15 +1,12 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    theme_gruvbox dark hard
+    direnv hook fish | source
 end
 
 set -g fish_greeting
-
-theme_gruvbox dark hard
 
 set -x ELECTRON_OZONE_PLATFORM_HINT wayland
 
 set -Ux EDITOR hx
 set -Ux VISUAL hx
 set -Ux SUDO_EDITOR
-
-direnv hook fish | source
