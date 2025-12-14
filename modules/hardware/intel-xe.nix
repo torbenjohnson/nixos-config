e8b92596907edc0b424ba20bfda47fc412583ccb
Torben Johnson <tjohnson@dimonoff.com>
2025-12-14 16:14:14 -0500

    disable Xe driver for intel igpu

--- modules/hardware/intel-xe.nix
+++ modules/hardware/intel-xe.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 
 {
-  boot.kernelParams = [
-    "i915.force_probe=!*"
-    "xe.force_probe=*"
-  ];
-  boot.blacklistedKernelModules = [ "i915" ];
+  # boot.kernelParams = [
+  #   "i915.force_probe=!*"
+  #   "xe.force_probe=*"
+  # ];
+  # boot.blacklistedKernelModules = [ "i915" ];
 
   hardware.graphics.extraPackages = with pkgs; [
     intel-media-driver
