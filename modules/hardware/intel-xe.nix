b92297d7d04dca49935dd1fd769305a3733ff3c5
Torben Johnson <tjohnson@dimonoff.com>
2026-06-05 16:01:49 -0400

    disable the Xe force probe

--- modules/hardware/intel-xe.nix
+++ modules/hardware/intel-xe.nix
@@ -1,12 +1,6 @@
 { pkgs, ... }:
 
 {
-  boot.kernelParams = [
-    "i915.force_probe=!*"
-    "xe.force_probe=*"
-  ];
-  boot.blacklistedKernelModules = [ "i915" ];
-
   hardware.graphics.extraPackages = with pkgs; [
     intel-media-driver
     vpl-gpu-rt
